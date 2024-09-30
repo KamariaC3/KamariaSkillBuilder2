cfdfe6f9078cbd5d6205985295103d68a7193437
kcureton23 <kcureton23@students.desu.edu>
2024-09-30 18:38:14 -0400

    Done

--- src/main/java/PowerPill.java
+++ src/main/java/PowerPill.java
@@ -10,21 +10,45 @@
 public class PowerPill
 {
     // TODO - replace this line with instruction from step 1
-
+    static int DEFAULT_POWER = 10;
+    int power;
     // instance variables
-    // TODO - replace this line with instruction from step 2
-    // TODO - replace this line with instruction from step 3
+    // TODO - replace this line with instruction from step
 
+    // TODO - replace this line with instruction from step 3
+    String name;
     // constructors
 
+    public PowerPill(String name){
+        this.name = name;
+        this.power = DEFAULT_POWER;
+    }
+
+    public PowerPill(String name, int power){
+        this.power = power;
+        this.name = name;
+    }
+
+    // Powerpill p = new Powerpill("purple")
     // TODO - replace this line with instruction from step 4
-
+    public int getPower(){
+        return power;
+    }
+    public String getName(){
+        return name;
+    }
     // TODO - replace this line with instruction from step 5
-
-
     // accessor methods
-
+    public void setPower(int power) {
+        this.power = power;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
     // TODO - replace this line with instruction from step 6
+    public String toString() {
+        return ("PowerPill " +  name + " = " + power);
+    }
 
 
     // mutator methods
